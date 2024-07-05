8c1cfa1ce65483c6176730616cd7e70ba2ba07ef
ScottCPetty <scottishcheese13@gmail.com>
2024-07-05 13:59:30 -0400

    seeded tables

--- server/index.js
+++ server/index.js
@@ -26,11 +26,34 @@ app.get("/api/restaurants", async (req, res, next) => {
     console.log(error);
   }
 });
-app.get("/api/reservations", async (req, res, next) => {});
-app.post("/api/customers/:id/reservations", async (req, res, next) => {});
+// app.get("/api/reservations", async (req, res, next) => {});
+app.post("/api/customers/:id/reservations", async (req, res, next) => {
+  try {
+    res.status(201).send(
+      await createReservation({
+        date: req.body.date,
+        party_count: req.body.party_count,
+        restaurant_id: req.body.restaurant_id,
+        customer_id: req.params.customer_id,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
 app.delete(
   "/api/customers/:customer_id/reservations/:id",
-  async (req, res, next) => {}
+  async (req, res, next) => {
+    try {
+      await destroyReservation({
+        id: req.params.id,
+        customer_id: req.params.customer_id,
+      });
+      res.sendStatus(204);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 );
 app.use((error, req, res, next) => {
   res.status(error.status || 500).send({ error: error });
@@ -41,5 +64,30 @@ const init = async () => {
   console.log("connected to database");
   await createTables();
   console.log("created tables");
+  const [Patrick, Paul, Barcadia, Dorsia] = await Promise.all([
+    createCustomer({ name: "Patrick" }),
+    createCustomer({ name: "Paul" }),
+    createRestaurant({ name: "Barcadia" }),
+    createRestaurant({ name: "Dorsia" }),
+  ]);
+  console.log(await fetchCustomers());
+  console.log(await fetchRestaurants());
+  const [reservation, reservation2] = await Promise.all([
+    createReservation({
+      date: "07/10/2024",
+      party_count: "2",
+      restaurant_id: Barcadia.id,
+      customer_id: Patrick.id,
+    }),
+    createReservation({
+      date: "07/8/2024",
+      party_count: "2",
+      restaurant_id: Dorsia.id,
+      customer_id: Paul.id,
+    }),
+  ]);
+  console.log(await fetchVacations());
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
 };
 init();
